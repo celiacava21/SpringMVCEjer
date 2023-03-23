f3064201b00d55cc2deca6d5c2cc4ea5fbfa54e0
celia <celia.cava18ruiz@gmail.com>
2023-03-23 18:47:11 +0100

    clases creadas

---
+++ src/main/java/com/example/SpringMvcEjerApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMvcEjerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMvcEjerApplication.class, args);
+	}
+
+}
