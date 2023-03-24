d23ba90f022426116cd568c06364e4fe71270915
celia <celia.cava18ruiz@gmail.com>
2023-03-24 09:01:45 +0100

    listar estudiantes correcto

--- src/main/java/com/example/controllers/MainController.java
+++ src/main/java/com/example/controllers/MainController.java
@@ -1,9 +1,14 @@
 package com.example.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
+import com.example.entities.Empleado;
 import com.example.services.CorreoService;
 import com.example.services.DepartamentoService;
 import com.example.services.EmpleadoService;
@@ -26,4 +31,17 @@ public class MainController {
     @Autowired
     private CorreoService correoService;
     
+    /***Método que devuelve listado de estudiantes */
+
+    @GetMapping("/listar")
+    public ModelAndView listar() {
+        List<Empleado> empleados = empleadoService.findAll();
+
+        ModelAndView mav = new ModelAndView("views/listarEmpleados");
+        mav.addObject("empleados", empleados);
+
+        return mav;
+    }
+
+
 }
