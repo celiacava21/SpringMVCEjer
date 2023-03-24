a63303467954d10f275a593e4d9f0c1d13405f6b
celia <celia.cava18ruiz@gmail.com>
2023-03-24 12:34:33 +0100

    actualizado correcto

--- src/main/java/com/example/controllers/MainController.java
+++ src/main/java/com/example/controllers/MainController.java
@@ -3,12 +3,15 @@ package com.example.controllers;
 import java.util.Arrays;
 import java.util.List;
 import java.util.logging.Logger;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -115,6 +118,41 @@ public class MainController {
         
     }
 
-    
+    /**Método que muestra el formulario que acutaliza un estudiante */
+
+    @GetMapping("/frmActualizar/{id}")
+    public String frmActualizarEmpleado(@PathVariable(name = "id")
+    int idEmpleado, Model model) {
+
+        Empleado empleado = empleadoService.findByÍd(idEmpleado);
+
+        List<Telefono> todosTelefonos = telefonoService.findAll();
+        List<Telefono> telefonosDelEmpleado = todosTelefonos.stream()
+        .filter(telefono -> telefono.getEmpleado().getId() == idEmpleado)
+        .collect(Collectors.toList());
+
+        String numerosDeTelefono = telefonosDelEmpleado.stream().map(telefono 
+        -> telefono.getNumero()).collect(Collectors.joining(";"));
+
+        List<Correo> todosCorreos = correoService.findAll();
+        List<Correo> correosDelEmpleado = todosCorreos.stream()
+        .filter(correo -> correo.getEmpleado().getId() == idEmpleado)
+        .collect(Collectors.toList());
+
+        String emailsDeCorreo = correosDelEmpleado.stream().map(correo 
+        -> correo.getEmail()).collect(Collectors.joining(";"));
+
+        List<Departamento> departamentos = departamentoService.findAll();
+
+        model.addAttribute("empleado", empleado);
+        model.addAttribute("telefonos", numerosDeTelefono);
+        model.addAttribute("correos", emailsDeCorreo);
+        model.addAttribute("departamentos", departamentos);
+        
+
+        return "views/formularioAltaEmpleado";
+
+        
+    }
 
 }
